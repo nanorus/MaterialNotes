b93a2f0947e863caeb539e36710d29370c77c2e7
nanorus <ruslantyshchenko@yandex.ru>
2017-06-05 18:58:09 +0300

    Improved Priority of the note. Now show color of a note depending of the Priority.

--- app/src/main/java/com/example/nanorus/todo/view/ui/adapters/NotesRecyclerViewAdapter.java
+++ app/src/main/java/com/example/nanorus/todo/view/ui/adapters/NotesRecyclerViewAdapter.java
@@ -1,9 +1,11 @@
 package com.example.nanorus.todo.view.ui.adapters;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.nanorus.nanojunior.R;
@@ -27,8 +29,24 @@ public class NotesRecyclerViewAdapter extends RecyclerView.Adapter<NotesRecycler
 
     @Override
     public void onBindViewHolder(NotesRecyclerViewHolder holder, int position) {
+        int priority = mNotes.get(position).getPriority();
         holder.name.setText(mNotes.get(position).getName());
-        holder.priority.setText(String.valueOf(mNotes.get(position).getPriority()));
+        holder.priority.setText(String.valueOf(priority));
+        switch (priority){
+            case 1:
+                holder.priority_color.setBackgroundColor(Color.parseColor("#E91E63"));
+                break;
+            case 2:
+                holder.priority_color.setBackgroundColor(Color.parseColor("#9C27B0"));
+                break;
+            case 3:
+                holder.priority_color.setBackgroundColor(Color.parseColor("#2196F3"));
+                break;
+            default:
+                holder.priority_color.setBackgroundColor(Color.parseColor("#2196F3"));
+                break;
+        }
+
     }
 
     @Override
@@ -39,11 +57,13 @@ public class NotesRecyclerViewAdapter extends RecyclerView.Adapter<NotesRecycler
     class NotesRecyclerViewHolder extends RecyclerView.ViewHolder {
         TextView name;
         TextView priority;
+        ImageView priority_color;
 
         public NotesRecyclerViewHolder(View itemView) {
             super(itemView);
             name = (TextView) itemView.findViewById(R.id.list_item_note_name);
             priority = (TextView) itemView.findViewById(R.id.list_item_note_tv_priority);
+            priority_color = (ImageView) itemView.findViewById(R.id.list_item_note_iv_priority_color);
         }
     }
 }
